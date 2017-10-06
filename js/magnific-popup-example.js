fa5e86e62259a32805edca3e7ed82006615c5072
oksymets <gakmareba@gmail.com>
2017-10-06 15:32:36 +0300

    test

---
+++ js/magnific-popup-example.js
@@ -0,0 +1,39 @@
+/** Created by oksymets on 10/6/17. */
+/** http://dimsemenov.com/plugins/magnific-popup */
+
+$(document).ready(function() {
+
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+
+    });
+
+    $('.image-popup-fit-width').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        image: {
+            verticalFit: false
+        }
+    });
+
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't forget to change the duration also in CSS
+        }
+    });
+
+});
